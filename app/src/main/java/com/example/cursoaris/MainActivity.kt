364e72f7d5b6cba3b338476e8624d1723e31864d
joaquinxtx <thejotex1998@gmail.com>
2023-03-25 14:16:43 -0300

    Second Commit

--- app/src/main/java/com/example/cursoaris/MainActivity.kt
+++ app/src/main/java/com/example/cursoaris/MainActivity.kt
@@ -1,34 +1,116 @@
 package com.example.cursoaris
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.slider.RangeSlider
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
     private var isMaleSelected:Boolean=true
     private var isFemaleSelected:Boolean=false
+    private var currentWeight:Int=70
+    private var currentAge:Int=12
 
     private lateinit var viewMale:CardView
     private lateinit var viewFemale:CardView
+    private lateinit var tvHeight:TextView
+    private lateinit var rsHeight:RangeSlider
+    private lateinit var btnSubtractWeight:FloatingActionButton
+    private lateinit var btnPlusWeight:FloatingActionButton
+    private lateinit var tvWight:TextView
+    private lateinit var btnSubtractAge:FloatingActionButton
+    private lateinit var btnPlusAge:FloatingActionButton
+    private lateinit var tvAge:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initComponents()
         initListeners()
+        initUI()
     }
+
 
 
     private fun initComponents() {
         viewMale = findViewById(R.id.viewMale)
         viewFemale = findViewById(R.id.viewFemale)
+        tvHeight=findViewById(R.id.tvHeight)
+        rsHeight=findViewById(R.id.rsHeight)
+        btnSubtractWeight=findViewById(R.id.btnSubtractWeight)
+        btnPlusWeight=findViewById(R.id.btnPlusWeight)
+        tvWight=findViewById(R.id.tvWeight)
+        btnSubtractAge=findViewById(R.id.btnSubtractAge)
+        btnPlusAge=findViewById(R.id.btnPlusAge)
+        tvAge=findViewById(R.id.tvAge)
     }
     private fun initListeners() {
-        viewMale.setOnClickListener{setGenderColor()}
-        viewFemale.setOnClickListener{setGenderColor()}
+        viewMale.setOnClickListener{
+            changeGender()
+            setGenderColor()
+        }
+        viewFemale.setOnClickListener{
+            changeGender()
+            setGenderColor()
+        }
+
+        rsHeight.addOnChangeListener { _, value, _ ->
+            val df =DecimalFormat("#.##")
+            val result = df.format(value)
+            tvHeight.text="$result cm"
+        }
+        btnPlusWeight.setOnClickListener{
+            currentWeight += 1
+            setWeight()
+        }
+        btnSubtractWeight.setOnClickListener{
+            currentWeight -= 1
+            setWeight()
+        }
+        btnPlusAge.setOnClickListener{
+            currentAge += 1
+            setAge()
+        }
+        btnSubtractAge.setOnClickListener{
+            currentAge -= 1
+            setAge()
+        }
     }
 
-    private fun setGenderColor(isViewSelected:Boolean){
+    private fun setAge() {
+        tvAge.text=currentAge.toString()
+    }
 
+    private fun setWeight() {
+        tvWight.text=currentWeight.toString()
+    }
+
+    private fun changeGender(){
+        isMaleSelected=!isMaleSelected
+        isFemaleSelected=!isFemaleSelected
+    }
+
+    private fun setGenderColor(){
+
+        viewMale.setBackgroundColor(getSelectBackgroundColor(isMaleSelected))
+        viewFemale.setBackgroundColor(getSelectBackgroundColor(isFemaleSelected))
+    }
+
+    private fun getSelectBackgroundColor(isSelectedComponents:Boolean):Int{
+        val colorReference = if(isSelectedComponents){
+             R.color.background_component_selected
+        }else{
+             R.color.background_component
+        }
+
+        return ContextCompat.getColor(this,colorReference)
+    }
+    private fun initUI() {
+        setGenderColor()
     }
 }
